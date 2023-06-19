f37e87711fb3ddcaff826d125e58399d2663a4ed
pczyz <pczyz@LAPTOP-G8A70BES>
2023-06-19 14:57:14 +0200

    Dodaj pliki projektów.

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit.Sdk;
+
+namespace WebApplication1.Forms
+{
+    public class FizzBuzzForm
+    {
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        [Range(1, 1000, ErrorMessage = "Oczekiwana wartość '{0}' z zakresu {1} i {2}")]
+        
+        public int Number { get; set; }
+        public string? AlertInfo { get; set; }
+        public string? AlertStyle { get; set; } //invisible alert alert-primary
+
+        public FizzBuzzForm()
+        {
+            AlertInfo = "";
+            AlertStyle = "invisible";
+        }
+        public void Check()
+        {
+            if (Number >= 1 && Number <= 1000)
+            {
+                if (Number % 3 == 0 && Number % 5 == 0)//Podzielna przez 3 i 5
+                {
+                    AlertInfo = "FizzBuzz";
+                    AlertStyle = "alert alert-success";
+                }
+                else if (Number % 3 == 0) //Podzielna przez 3
+                {
+                    AlertInfo = "Fizz";
+                    AlertStyle = "alert alert-success";
+                }
+                else if (Number % 5 == 0)//Podzielna przez 5
+                {
+                    AlertInfo = "Buzz";
+                    AlertStyle = "alert alert-success";
+                }
+                else //W innych przypadkach
+                {
+                    AlertInfo = "Liczba: " + Number.ToString() + " nie spełnia kryteriów FizzBuzz";
+                    AlertStyle = "alert alert-danger";
+                }
+            }
+        }
+    }
+
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Forms;
+
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+
+        public FizzBuzzForm FizzBuzz { set; get; }
+        [BindProperty(SupportsGet = true)]
+
+        public String Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                FizzBuzz.Check();
+            }
+            return Page();
+        }
+    }
+}
